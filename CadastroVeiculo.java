379f08a3e28391f15585fd1d400ca582119f7f07
Samu <127152668+WiniciusNeves@users.noreply.github.com>
2023-04-14 20:17:32 -0300

    Create CadastroVeiculo.java

---
+++ CadastroVeiculo.java
@@ -0,0 +1,24 @@
+# faculty
+code i learned in college
+import java.util.Scanner;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Cadastro de Veiculos");
+		
+		for (int indice = 0; indice<5; indice++) {
+			
+			System.out.println("Digite o nome do veiculo");
+			String nomeVeiculos = scanner.nextLine();
+			
+		
+		}
+		
+		
+	}
+
+}
