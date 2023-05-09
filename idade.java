e1ba68064a90c83f507b6119480892a42602f795
Samu <127152668+WiniciusNeves@users.noreply.github.com>
2023-05-09 15:01:47 -0300

    idade.java

---
+++ idade.java
@@ -0,0 +1,34 @@
+package br.com.unicuritiba;
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner idade01 = new Scanner(System.in);
+		int cont = 1;
+		int idadenum=0;
+		int MM20=0;
+		String sexo;
+		for(int i=0; i<=10;i++ ) {
+			System.out.println("Pessoa: "+cont+"\n");
+			System.out.println("informe seu idade:");
+			int idade = idade01.nextInt();
+		idade01.nextLine();
+		System.out.println("Sexo(Masculino/ferminino), por favor so coloca o inicial de cada um delas");
+		sexo = idade01.nextLine();
+		idadenum+= idade;
+		cont++;
+		if(sexo==("F")&& idade >20) {
+			MM20++;
+		}
+	}
+		double media = idadenum / 10.0; 
+		System.out.println("A media é: "+media+"\n");
+		System.out.println("As mulheres com mais de 20 anos: "+MM20+"\n");
+		
+	
+		
+	}
+
+}
