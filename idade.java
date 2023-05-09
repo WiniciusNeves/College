5b972c33b6127ed346383a55f22cc5eb04fe3912
Samu <127152668+WiniciusNeves@users.noreply.github.com>
2023-05-09 17:13:49 -0300

    idade.java

--- idade.java
+++ idade.java
@@ -10,24 +10,22 @@ public class Main {
 		int idadenum=0;
 		int MM20=0;
 		String sexo;
-		for(int i=0; i<=10;i++ ) {
-			System.out.println("Pessoa: "+cont+"\n");
-			System.out.println("informe seu idade:");
+		for(int i=0; i<3;i++ ) {
+			System.out.println("Pessoa: "+cont);
+			System.out.print("informe seu idade:");
 			int idade = idade01.nextInt();
 		idade01.nextLine();
 		System.out.println("Sexo(Masculino/ferminino), por favor so coloca o inicial de cada um delas");
 		sexo = idade01.nextLine();
 		idadenum+= idade;
 		cont++;
-		if(sexo==("F")&& idade >20) {
+		 if (sexo.equalsIgnoreCase("F") && idade > 20) {
 			MM20++;
 		}
 	}
 		double media = idadenum / 10.0; 
 		System.out.println("A media é: "+media+"\n");
 		System.out.println("As mulheres com mais de 20 anos: "+MM20+"\n");
-		
-	
 		
 	}
 
