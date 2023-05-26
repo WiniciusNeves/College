41fe9919b498f31f436f75244a88d02f4a8e74f6
Samu <127152668+WiniciusNeves@users.noreply.github.com>
2023-05-26 20:07:10 -0300

    calculatora.java

--- calculatora.java
+++ calculatora.java
@@ -1,48 +1,72 @@
-public class MyClass {
-    import java.util.Scanner;
-    public static void main(String args[]) {
-     Scanner scanner = new Scanner(System.in);
-     System.out.println("digite a operação que deseja:");
-     System.out.println("1 - Soma");
-     System.out.println("2 - Subtração");
-     System.out.println("3 - multiplicação")
-     System.out.println("4 - divisão")
+package br.com.unicuritiba;
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner scanner = new Scanner(System.in);
+	     System.out.println("digite a operação que deseja:");
+	     System.out.println("1 - Soma");
+	     System.out.println("2 - Subtração");
+	     System.out.println("3 - multiplicação");
+	     System.out.println("4 - divisão");
 
 
-        int opcao = scanner.nextInt();
-        
-        
-        System.out.println("Digite o operando 1");
-        
-        int operando1 = scanner.nextInt();
-        
-        System.out.println("Digite o operando 2");
-        
-        int operando2 = scanner.nextInt();
-        
-        
-        switch(opcao){
-            case 1:
-                
-                int resultado = operando1 + operando2;
-                
-                break;
-            
-            case 2;
-                break;
-            
-            case 3;    
-                break;
-                
-            case 4;
-                break;
-        
-            default:
-                break
-        }
-        
+	        int opcao = scanner.nextInt();
+	        
+	        
+	        System.out.println("Digite o operando 1");
+	        
+	        int operando1 = scanner.nextInt();
+	        
+	        System.out.println("Digite o operando 2");
+	        
+	        int operando2 = scanner.nextInt();
+	        
+	        
+	        switch(opcao){
+	            case 1:
+	                
+	               System.out.println(somar(operando1 , operando2));
+	                break;
+	            
+	            case 2:
+		              System.out.println(sub(operando1 , operando2));
+	                break;
+	            
+	            case 3: 
+	            	System.out.println(mult(operando1 , operando2));
+	                break;
+	                
+	            case 4:
+	            	System.out.println(divit(operando1 , operando2));
+	                break;
+	        
+	            default:
+	                break;
+	        }
+	        
+	}
+	        
 
-        
-    }
-    
+	    private static int somar(int num1, int num2) {
+		    int resultado = num1 + num2;
+		    return resultado;
+	    }
+	    
+	    private static int sub(int num1, int num2) {
+		    int resultado = num1 - num2;
+		    return resultado;	
+	    }
+
+	    private static int mult(int num1, int num2) {
+		    int resultado = num1 * num2;
+		    return resultado;
+	    }
+
+	    private static int divit(int num1, int num2) {
+		    int resultado = num1 / num2;
+		    return resultado;
+	    }
+
 }
