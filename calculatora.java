5f07c134db10781bef77edc48d7a37eb837589ae
Samu <127152668+WiniciusNeves@users.noreply.github.com>
2023-09-21 19:47:02 -0300

    Update calculatora.java

--- calculatora.java
+++ calculatora.java
@@ -1,90 +1,81 @@
-package br.com.unicuritiba;
 import java.util.Scanner;
-public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		 Scanner scanner = new Scanner(System.in);
-		int resultado = 0;
-	     System.out.println("digite a operação que deseja:");
-	     System.out.println("1 - Soma");
-	     System.out.println("2 - Subtração");
-	     System.out.println("3 - multiplicação");
-	     System.out.println("4 - divisão");
+public class calculadora {
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int resultado = 0;
+        System.out.println("Digite a operação que deseja:");
+        System.out.println("1 - Soma");
+        System.out.println("2 - Subtração");
+        System.out.println("3 - Multiplicação");
+        System.out.println("4 - Divisão");
 
-	        int opcao = scanner.nextInt();
-	        
-	        
-	        System.out.println("Digite o operando 1");
-	        
-	        int operando1 = scanner.nextInt();
-	        
-	        System.out.println("Digite o operando 2");
-	        
-	        int operando2 = scanner.nextInt();
-	        
-	        
-	        switch(opcao){
-	            case 1:
-	                
-	               System.out.println(somar(operando1 , operando2));
-	                break;
-	            
-	            case 2:
-		              System.out.println(sub(operando1 , operando2));
-	                break;
-	            
-	            case 3: 
-	            	System.out.println(mult(operando1 , operando2));
-	                break;
-	                
-	            case 4:
-	            	System.out.println(divit(operando1 , operando2));
-	                break;
-	        
-	            default:
-				System.out.println("operacao invalida");
-	                break;
-	        }
-	        if(validarResultado(resultado)){
-			System.out.printl("o resultado é "+ resultado)
-		}
-		else{
-			System.out.println("o resultado nao e valido")
-		}
-	}
-	
-	        
+        int opcao = scanner.nextInt();
 
-	    private static int somar(int num1, int num2) {
-		    int resultado = num1 + num2;
-		    return resultado;
-	    }
-	    
-	    private static int sub(int num1, int num2) {
-		    int resultado = num1 - num2;
-		    return resultado;	
-	    }
+        System.out.println("Digite o operando 1");
+        int operando1 = scanner.nextInt();
 
-	    private static int mult(int num1, int num2) {
-		    int resultado = num1 * num2;
-		    return resultado;
-	    }
+        System.out.println("Digite o operando 2");
+        int operando2 = scanner.nextInt();
 
-	    private static int divit(int num1, int num2) {
-		    int resultado = num1 / num2;
-		    return resultado;
-	    }
-	    
-	    private static boolean erro (int resultado ) {
-		    if (resultado >= 300) {
-		    	return true;
-		    }
-		    else 
-		    	return false;
-		    }
-		    
-	    
-	    
+        switch (opcao) {
+            case 1:
+                resultado = somar(operando1, operando2);
+                break;
+
+            case 2:
+                resultado = subtrair(operando1, operando2);
+                break;
+
+            case 3:
+                resultado = multiplicar(operando1, operando2);
+                break;
+
+            case 4:
+                if (operando2 != 0) {
+                    resultado = dividir(operando1, operando2);
+                } else {
+                    System.out.println("Erro: divisão por zero.");
+                    return;
+                }
+                break;
+
+            default:
+                System.out.println("Operação inválida");
+                return;
+        }
+
+        System.out.println("O resultado é: " + resultado);
+
+        if (erro(resultado)) {
+            System.out.println("O resultado é maior ou igual a 300.");
+        } else {
+            System.out.println("O resultado não é válido.");
+        }
+    }
+
+    private static int somar(int num1, int num2) {
+        int resultado = num1 + num2;
+        return resultado;
+    }
+
+    private static int subtrair(int num1, int num2) {
+        int resultado = num1 - num2;
+        return resultado;
+    }
+
+    private static int multiplicar(int num1, int num2) {
+        int resultado = num1 * num2;
+        return resultado;
+    }
+
+    private static int dividir(int num1, int num2) {
+        int resultado = num1 / num2;
+        return resultado;
+    }
+
+    private static boolean erro(int resultado) {
+        return resultado >= 300;
+    }
 }
